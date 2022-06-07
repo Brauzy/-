cb5a8ae0f0ec83e83c03da79183f12d636eb5454
Brauzy <101868074+Brauzy@users.noreply.github.com>
2022-06-07 22:14:48 +0300

    Add files via upload

---
+++ Proekt/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Card from './Card';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Card title="Card Title" description="Card Description" />
+            <Card title="Card Title" description="Card Description" />
+            <Card title="Card Title" description="Card Description" />
+            <Card title="Card Title" description="Card Description" />
+            <Card title="Card Title" description="Card Description" />
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#03fff2',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
